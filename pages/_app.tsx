2801df6ac8cf0a986209bfc30ef453aad3c6056c
yoratoni <yoratoni.dev@gmail.com>
2025-03-24 20:14:39 +0100

    Added an automated redirection (302) to the homepage if current page is not found

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,14 @@ App.getInitialProps = async (appContext: NextAppContext) => {
 	const appProps = await NextApp.getInitialProps(appContext)
 	const nonce = appContext.ctx.req?.headers?.["x-nonce"] as string | undefined
 
+	// Auto-redirect to home page if the requested page is not found
+	// while also letting the crawler know that the page is not found
+	// and that it is a 302 redirect
+	if (appContext.ctx.res?.statusCode === 404) {
+		appContext.ctx.res.writeHead(302, { location: "/" })
+		appContext.ctx.res.end()
+	}
+
 	return {
 		...appProps,
 		nonce,
